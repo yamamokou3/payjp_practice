86ccfa7257e4dc1933bb1a7c7565f2d07c6ff2ef
yamamokou3 <alsk.89vbm25@gmail.com>
2020-11-15 23:09:00 +0900

    決済処理の実装

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Order, type: :model do
     @order = FactoryBot.build(:order)
   end
 
-  it "priceがあれば保存ができること" do
+  it "priceとtokenがあれば保存ができること" do
     expect(@order).to be_valid
   end
 
@@ -13,5 +13,11 @@ RSpec.describe Order, type: :model do
     @order.price = nil
     @order.valid?
     expect(@order.errors.full_messages).to include("Price can't be blank")
+  end
+
+  it "tokenが空では登録できないこと" do
+    @order.token = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Token can't be blank")
   end
 end
